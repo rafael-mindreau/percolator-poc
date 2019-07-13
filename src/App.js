dc1968b0c9a82908b9dc9e18709b7c0bdf5e3607
Rafael <rafael.mindreau@gmail.com>
2019-07-13 22:31:41 +0200

    Making the Router work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Route, Switch, NavLink } from 'react-router-dom';
+import Home from './components/Home';
+import Configurator from './components/Configurator';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/configurator">Configurator</NavLink>
+            </li>
+          </ul>
+        </nav>
       </header>
+
+      <Switch>
+        <Route exact path="/" component={Home}>Home</Route>
+        <Route path="/configurator" component={Configurator}>Configurator</Route>
+      </Switch>
+
+      <footer />
+
     </div>
   );
 }
