62c086d3e52d487d191ee923adb47febb9471498
Rafael <rafael.mindreau@gmail.com>
2019-07-15 11:47:08 +0200

    Add a colors constant

---
+++ src/constants/colors.js
@@ -0,0 +1,10 @@
+export default {
+  primaryBackground: '#1F180F',
+  secondaryBackground: '#33312C',
+  lightForeground: '#C0BCA9',
+  mainColor: '#402A17',
+  callToAction: '#ffc700',
+
+  darkText: '#080500',
+  lightText: '#eceae2',
+};
